574396934568f9ca60bfb28bf47a8a1fbb3dfa52
Etim Israel <israeletim4@gmail.com>
2025-03-24 14:50:57 +0100

    Done with the project using RESTful pattern

--- index.js
+++ index.js
@@ -41,14 +41,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const identity = parseInt(req.params.id);
+  const post = posts.find((getPost) => getPost.id === identity);
+  if (!post) return res.status(401).json({ message: "Identity not found!"})
+  res.json(post);
+});
 
 //CHALLENGE 3: POST a new post
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const identity = parseInt(req.params.id);
+  const mainPost = posts.find((post) => post.id === identity);
+  const postData = {
+    title: req.body.title || mainPost.title,
+    content: req.body.content || mainPost.content,
+    author: req.body.author || mainPost.author,
+  };
+  const searchIndex = posts.findIndex((joke) => joke.id === identity);
+
+  posts[searchIndex] = postData;
+  res.status(201).json(postData);
+});
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const identity = parseInt(req.params.id);
+  const mainPost = posts.findIndex((post) => post.id === identity);
+
+  if (mainPost > -1) {
+    posts.splice(mainPost, 1)
+    res.status(201).json({ error: `Post Content ${identity} deleted!`})
+  } else {
+    res.status(401).json({ error: `Post Content ${identity} not deleted!`})
+  }
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
